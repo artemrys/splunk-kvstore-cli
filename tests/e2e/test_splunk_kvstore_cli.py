2de8d33847848f31d879bc09cf0244bf8746fd68
Artem Rys <rysartem@gmail.com>
2022-04-04 22:34:24 +0200

    Run pre-commit

--- tests/e2e/test_splunk_kvstore_cli.py
+++ tests/e2e/test_splunk_kvstore_cli.py
@@ -3,13 +3,14 @@ import os
 
 import splunk_kvstore_cli
 
-
 SPLUNK_USERNAME = "admin"
 SPLUNK_PASSWORD = os.getenv("SPLUNK_PASSWORD")
 
 
 def test_kv_login():
-    splunk_kvstore_cli.login(f"{SPLUNK_USERNAME}:{SPLUNK_PASSWORD}", host="https://localhost:8089")
+    splunk_kvstore_cli.login(
+        f"{SPLUNK_USERNAME}:{SPLUNK_PASSWORD}", host="https://localhost:8089"
+    )
     credentials_path = os.path.join(os.path.expanduser("~"), ".splunk_kvstore_creds")
     with open(credentials_path) as f:
         content = f.read()
@@ -17,7 +18,9 @@ def test_kv_login():
 
 
 def test_kv_get_config_when_no_collection():
-    splunk_kvstore_cli.login(f"{SPLUNK_USERNAME}:{SPLUNK_PASSWORD}", host="https://localhost:8089")
+    splunk_kvstore_cli.login(
+        f"{SPLUNK_USERNAME}:{SPLUNK_PASSWORD}", host="https://localhost:8089"
+    )
     result = splunk_kvstore_cli.get_config("search", "default")
     parsed = json.loads(result)
     assert len(parsed) == 1
